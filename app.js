1e5e213fc94f6224e074b1cedbf7a0a1c64f4300
Bombale <amruth.parasuram@bt.com>
2019-07-19 16:11:26 +0530

    Change greeting message

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
+const TelegramBot = require('node-telegram-bot-api');
 const http = require('http');
+
+const token = '923352008:AAGigsiG3IApxMLmsb8M_PGRlvT757IhBuk';
+
 const port = process.env.PORT || 3000
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
@@ -7,34 +11,29 @@ const server = http.createServer((req, res) => {
 });
 server.listen(port, () => {
     console.log(`Server running at port ` + port);
-});
+    // Create a bot that uses 'polling' to fetch new updates
+    const bot = new TelegramBot(token, { polling: true });
 
-const TelegramBot = require('node-telegram-bot-api');
+    // Matches "/echo [whatever]"
+    bot.onText(/\/echo (.+)/, (msg, match) => {
+        // 'msg' is the received Message from Telegram
+        // 'match' is the result of executing the regexp above on the text content
+        // of the message
 
-// replace the value below with the Telegram token you receive from @BotFather
-const token = '923352008:AAGigsiG3IApxMLmsb8M_PGRlvT757IhBuk';
+        const chatId = msg.chat.id;
+        const resp = match[1]; // the captured "whatever"
 
-// Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(token, {polling: true});
+        // send back the matched "whatever" to the chat
+        bot.sendMessage(chatId, resp);
+    });
 
-// Matches "/echo [whatever]"
-bot.onText(/\/echo (.+)/, (msg, match) => {
-  // 'msg' is the received Message from Telegram
-  // 'match' is the result of executing the regexp above on the text content
-  // of the message
+    // Listen for any kind of message. There are different kinds of
+    // messages.
+    bot.on('message', (msg) => {
+        const chatId = msg.chat.id;
 
-  const chatId = msg.chat.id;
-  const resp = match[1]; // the captured "whatever"
+        // send a message to the chat acknowledging receipt of their message
+        bot.sendMessage(chatId, 'Received your message, dude.');
+    });
 
-  // send back the matched "whatever" to the chat
-  bot.sendMessage(chatId, resp);
-});
-
-// Listen for any kind of message. There are different kinds of
-// messages.
-bot.on('message', (msg) => {
-  const chatId = msg.chat.id;
-
-  // send a message to the chat acknowledging receipt of their message
-  bot.sendMessage(chatId, 'Received your message');
 });
