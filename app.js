316c66e32c0c250c171d3f2a157f5c223cb18cf3
Bombale <amruth.parasuram@bt.com>
2019-07-21 17:00:25 +0530

    Add dailyResults endpoint

--- app.js
+++ app.js
@@ -1,35 +1,61 @@
 const dateutil = require('./utils/dateutil');
+const reqHelper = require('./utils/request-helper');
+
 const TelegramBot = require('node-telegram-bot-api');
 const express = require('express');
 const ejs = require('ejs');
+
+const resultsUrl = 'https://tools.traderslounge.in/fnoresults';
+
+const token = '923352008:AAGigsiG3IApxMLmsb8M_PGRlvT757IhBuk';
+const channelChatId = '-1001453070196';
+const bot = new TelegramBot(token, { polling: true });
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(express.static('public'));
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
-    const bot = new TelegramBot(token, { polling: true });
-    bot.sendMessage(channelChatId, 'Today\'s Expected Results'); // TODO: Replace with stocks for the day.
-
     bot.on('message', (msg) => {
         const chatId = msg.chat.id;
         bot.sendMessage(chatId, 'I don\'t do anything. Join https://t.me/joinchat/AAAAAFacF3TKxasORZyjpQ');
     });
-
     res.render('index');
 });
 
-const token = '923352008:AAGigsiG3IApxMLmsb8M_PGRlvT757IhBuk';
-const channelChatId = '-1001453070196';
 
 app.get('/checkResult', function (req, res) {
-    const bot = new TelegramBot(token, { polling: true });
     var date = dateutil.getDate();
     bot.sendMessage(channelChatId, 'As of ' + date + ' no results.'); // TODO: Replace with latest results.
     res.render('index');
 });
+
+app.get('/todayResults', async function (req, res) {
+    var results = await reqHelper.downloadPage(resultsUrl, true);
+    var nowDate = dateutil.getDate();
+    nowDate.setTime(nowDate.getTime());
+    for (var i = 0; i < results.length; i++) {
+        var result = results[i];
+        var resultsDate = new Date(result.date);
+        if (resultsDate.getDate() == nowDate.getDate() && resultsDate.getMonth() == nowDate.getMonth() && resultsDate.getFullYear() == nowDate.getFullYear()) {
+            var message = createMessage(result);
+            bot.sendMessage(channelChatId, message);
+            return;
+        }
+    }
+    bot.sendMessage(channelChatId, 'No results on ' + nowDate);
+});
+
+function createMessage(result) {
+    var message = result.title + '\n';
+    var symbols = result.Symbol;
+    for (var i = 0; i < symbols.length; i++) {
+        message += symbols[i] + '\n';
+    }
+    return message;
+}
 
 app.listen(PORT, function () {
     console.log('Example app listening on port ' + PORT + '!');
