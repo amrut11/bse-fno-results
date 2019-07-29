26513f1dfffae70f09e7105b155f2e7d253e77d0
Amrut <amrut11@gmail.com>
2019-07-29 10:37:39 +0530

    Correct case of alphavantage

--- app.js
+++ app.js
@@ -121,7 +121,7 @@ function startBot() {
     bot.onText(/\/atr (.+)/, async (msg, match) => {
         const chatId = msg.chat.id;
         const stockSymbol = match[1];
-        var atrUrl = process.env.atrApi.replace('{symbol}', stockSymbol).replace('{alphavantageKey}', process.env.alphaVantageKey);
+        var atrUrl = process.env.atrApi.replace('{symbol}', stockSymbol).replace('{alphavantageKey}', process.env.alphavantageKey);
         var response = await reqHelper.downloadPage(atrUrl);
         var atrJson = response['Technical Analysis: ATR'];
         console.dir(atrUrl);
