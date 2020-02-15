f6f383059fb770863d1f6a9c1832db4bb5bb8dc2
Amrut <amrut11@gmail.com>
2020-02-16 04:58:31 +0530

    Remove example command from bot.

--- bot/bot.js
+++ bot/bot.js
@@ -13,33 +13,33 @@ function startBot() {
 
     bot.onText(/[Rr]esults (.+)/, async (msg, match) => {
         const chatId = msg.chat.id;
+        dbService.audit(BOT, 'results ' + match[1], chatId);
         if (!process.env.authorisedUsers.includes(chatId)) {
             bot.sendMessage(chatId, 'Sorry. You\'re not authorised to use this. Please contact @NFOResult if you require access.');
             return;
         }
-        dbService.audit(BOT, 'results ' + match[1], chatId);
         var resultsDate = dateUtil.getDateFromMessage(match[1]);
         await resultsService.sendResultsMessage(bot, chatId, resultsDate);
     });
 
     bot.onText(/[Aa]nnounced (.+)/, async (msg, match) => {
         const chatId = msg.chat.id;
+        dbService.audit(BOT, 'announced ' + match[1], chatId);
         if (!process.env.authorisedUsers.includes(chatId)) {
             bot.sendMessage(chatId, 'Sorry. You\'re not authorised to use this. Please contact @NFOResult if you require access.');
             return;
         }
-        dbService.audit(BOT, 'announced ' + match[1], chatId);
         var resultsDate = dateUtil.getDateFromMessage(match[1]);
         await sendAnnouncedMessage(bot, chatId, resultsDate);
     });
 
     bot.onText(/[Cc]heck (.+)/, async (msg, match) => {
         var chatId = msg.chat.id;
+        dbService.audit(BOT, 'check ' + match[1], chatId);
         if (!process.env.authorisedUsers.includes(chatId)) {
             bot.sendMessage(chatId, 'Sorry. You\'re not authorised to use this. Please contact @NFOResult if you require access.');
             return;
         }
-        dbService.audit(BOT, 'check ' + match[1], chatId);
         const interval = match[1];
         await resultsService.checkResults(bot, chatId, interval);
     });
@@ -52,29 +52,15 @@ function startBot() {
 
     bot.onText(/[Aa]tr (.+)/, async (msg, match) => {
         const chatId = msg.chat.id;
+        dbService.audit(BOT, 'atr ' + match[1], chatId);
         if (!process.env.authorisedUsers.includes(chatId)) {
             bot.sendMessage(chatId, 'Sorry. You\'re not authorised to use this. Please contact @NFOResult if you require access.');
             return;
         }
-        dbService.audit(BOT, 'atr ' + match[1], chatId);
         const input = match[1].split(' ');
         const stockSymbol = input[0];
         var dateToCheck = dateUtil.getDateFromMessage(input[1]);
         await atrService.sendAtr(bot, chatId, stockSymbol, dateToCheck);
-    });
-
-    bot.onText(/Example/, (msg, match) => {
-        var options = {
-            reply_markup: JSON.stringify({
-                inline_keyboard: [
-                    [{ text: 'Company Announcement', url: 'http://www.google.com' }]
-                ]
-            }),
-            parse_mode: 'markdown'
-        };
-        var message = '*' + 'Results today' + '*\n-----------------------------------\n';
-        message += 'INFY';
-        bot.sendMessage(msg.chat.id, message, options);
     });
 
 }
